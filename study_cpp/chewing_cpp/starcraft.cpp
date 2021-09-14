e516d6ff5e7bb38b12ae10d1f0b8e222347ad564
dw <devdw98@gmail.com>
2021-09-14 14:53:01 +0900

    stacraft_초기

---
+++ study_cpp/chewing_cpp/starcraft.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+class Marine
+{
+    int hp;               // 체력
+    int coord_x, coord_y; // 위치
+    int damage;           // 공격력
+    bool is_dead;
+
+public:
+    Marine();             //default constructor
+    Marine(int x, int y); // x, y 좌표에 마린 생성
+
+    int attack();                      // 데이미 입히기
+    void be_attacked(int damage_earn); //얻은 데미지
+    void move(int x, int y);           // 위치 이동
+
+    void show_status(); // 마린 상태 보여주기
+};
+Marine::Marine()
+{
+    hp = 50;
+    coord_x = coord_y = 0;
+    damage = 5;
+    is_dead = false;
+}
+Marine::Marine(int x, int y)
+{
+    coord_x = x;
+    coord_y = y;
+    hp = 50;
+    damage = 5;
+    is_dead = false;
+}
+int Marine::attack()
+{
+    return damage;
+}
+void Marine::be_attacked(int damage_earn)
+{
+    hp -= damage_earn;
+    if (hp <= 0)
+        is_dead = true;
+}
+void Marine::move(int x, int y)
+{
+    coord_x = x;
+    coord_y = y;
+}
+void Marine::show_status()
+{
+    std::cout << "*** Marine ***" << std::endl;
+    std::cout << "Location : ( " << coord_x << ", " << coord_y << " ) " << std::endl;
+    std::cout << "HP: " << hp << std::endl;
+}
+
+int main()
+{
+    Marine marine1(2, 3);
+    Marine marine2(3, 5);
+
+    marine1.show_status();
+    marine2.show_status();
+
+    std::cout << std::endl
+              << "마린 1이 마린 2ㅣ 공격!" << std::endl;
+    marine2.be_attacked(marine1.attack());
+    marine1.show_status();
+    marine2.show_status();
+}
