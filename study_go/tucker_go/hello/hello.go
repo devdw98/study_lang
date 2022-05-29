97de6558984b5483acc4a127cb0a2abc1e8410e8
dowonpark <devdw98@gmail.com>
2022-05-29 21:02:58 +0900

    기초 코멘트

--- study_go/tucker_go/hello/hello.go
+++ study_go/tucker_go/hello/hello.go
@@ -1,6 +1,8 @@
-package main
+package main // 이 코드가 어떤 패키지에 속하는지 알려줌 - 반드시 패키지 선언으로 코드 시작해야함
+// main 패키지는 프로그램의 시작점을 포함하는 특별한 패키지임 - main함수가 포함되어야 함
+// main 함수가 없는 패키지는 실행파일을 만들수 없고, 다른 패키지에서 외부 패키지로 사용됨
 
-import "fmt"
+import "fmt" // 패키지 가져오기
 
 func main() {
 	fmt.Println("Hello World!")
---
+++ study_go/tucker_go/hello2/go.mod
@@ -0,0 +1,3 @@
+module tucker_go/hello2
+
+go 1.18
